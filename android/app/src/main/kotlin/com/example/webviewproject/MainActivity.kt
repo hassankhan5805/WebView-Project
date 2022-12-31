c42a17ab57f42401573dece6bf8ee0c73a68a29b
muhammad hassan <hassankhan5805@gmail.com>
2022-12-31 22:28:29 +0500

    webview ready

---
+++ android/app/src/main/kotlin/com/example/webviewproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.webviewproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "韩中九九直播",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+  // late WebViewController controller;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: WebView(
+        javascriptMode: JavascriptMode.unrestricted,
+        initialUrl: 'http://www.1977999999.com/',
+      )),
+    );
+  }
+}
